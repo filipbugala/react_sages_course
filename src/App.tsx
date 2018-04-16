2ae19398ac933b82e70fd89838a45a070ec84697
Mateusz Kulesza <eduwebgit@gmail.com>
2018-04-16 14:15:57 +0200

    Todos, events

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import { Todo } from './todos/todo';
 import { ChangeEvent } from 'react';
 // import logo from './logo.svg';
 
-interface State{
-  todos:Todo[], newTitle:string, counter:number
+interface State {
+  todos: Todo[],
+  newTitle: string
 }
 
-class App extends React.Component<{},State> {
+class App extends React.Component<{}, State> {
 
   state: State = {
     todos: [
@@ -18,24 +19,33 @@ class App extends React.Component<{},State> {
         completed: false
       }
     ],
-    newTitle: '',
-    counter: 0
+    newTitle: ''
   }
 
+  addTodo() {
+    const todo: Todo = {
+      id: Date.now(),
+      title: this.state.newTitle,
+      completed: false
+    }
+
+    // zle:
+    // this.state.todos.push(todo)
+
+    this.setState(prevState => ({
+      // todos: prevState.todos.concat(todo)
+      todos: [...prevState.todos, todo],
+      newTitle: ''
+    }))
+
+  }
 
   titleChange(event: ChangeEvent<HTMLInputElement>) {
     // event.persist()
     const value = event.target.value;
 
-    
     this.setState({
       newTitle: value
-    })
-
-    this.setState(prevState => {
-      return {
-        counter: prevState.newTitle.length
-      }
     })
   }
 
@@ -49,10 +59,12 @@ class App extends React.Component<{},State> {
             <TodoList todos={this.state.todos} />
 
             <div className="input-group">
-              <input type="text" className="form-control" onChange={e => this.titleChange(e)} />
-              <button className="btn">Add</button>
+              <input type="text" className="form-control"
+                value={this.state.newTitle}
+                onKeyUp={e => e.key == "Enter" && this.addTodo()}
+                onChange={e => this.titleChange(e)} />
+              <button className="btn" onClick={e => this.addTodo()}>Add</button>
             </div>
-            {this.state.counter}
 
           </div>
         </div>
