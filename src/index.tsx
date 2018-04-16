b9efa7009f02c9f585fd59503c0e4567fc1646fc
Mateusz Kulesza <eduwebgit@gmail.com>
2018-04-16 10:56:53 +0200

    React syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// import App from './App';
+// import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root') as HTMLElement
+// );
+// registerServiceWorker();
+
+const lista = [1, 2, 3, 4, 5].reverse()
+
+
+setInterval(() => {
+  lista.unshift(lista.length + 1)
+
+
+  const section = React.createElement('div', {},
+  React.createElement('h1', null, 'Todos'),
+  React.createElement('ul', null,
+    lista.map(x => React.createElement('li', {
+      key: x,
+      className: "list-item",
+      style: { color: x %2 == 0? 'red' : 'black' } 
+    }, 'Item ' + x))
+  ),
+  )
+
+  ReactDOM.render(section, document.getElementById('root'))
+}, 500)
+
+
+
+
+window['React'] = React;
+window['ReactDOM'] = ReactDOM
+
